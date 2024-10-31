ce941a80f2d393b04b2e6ff7d08acef1efd40275
SamratEmily <nayakemily50@gmail.com>
2024-10-31 17:04:48 +0600

    operation updated

---
+++ basics/03_conversions.js
@@ -0,0 +1,14 @@
+let userId = undefined;
+
+userId = String(userId);
+// console.log(typeof userId);
+// console.log(userId);
+
+// "undefined" => Number(variable) => Nan
+// "undefined" => String(variable) => undefined
+
+let gameCounter = 100;
+
+// console.log(gameCounter);
+
+console.table([gameCounter++, ++gameCounter, ++gameCounter, ++gameCounter]);
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,9 +1,9 @@
-"use strict";   // traet all Js code as newer version
+"use strict"; // traet all Js code as newer version
 
 // allert(3+3) we are using nodejs , not browser
 
-console.log(3+3)    
-console.log(typeof "5")
+console.log(3 + 3);
+console.log(typeof "5");
 
-console.log(typeof null)
-console.log(typeof undefined) 
+console.log(typeof null);
+console.log(typeof undefined);
