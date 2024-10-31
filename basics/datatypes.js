23a974a3bf71c33fae647ea2bfa5f5bed4e1f542
Samrat Hossen <60014791+SamratEmily@users.noreply.github.com>
2024-10-31 05:56:52 +0000

    data types

---
+++ basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict";   // traet all Js code as newer version
+
+// allert(3+3) we are using nodejs , not browser
+
+console.log(3+3)    
+console.log(typeof "5")
+
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ basics/first.js
@@ -0,0 +1,11 @@
+const accountId = 1001
+let accountEmail = "this@gmail.com"
+let emailPass = "1234"
+
+console.log(accountEmail)
+
+/**
+ * prefer not to use var due to it's block space
+ */
+
+console.table([accountEmail, emailPass, 123])
