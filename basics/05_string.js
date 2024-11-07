becfabc66fc1613665684d616be4d637a712ba3e
SamratEmily <nayakemily50@gmail.com>
2024-11-07 10:16:24 +0600

    praticing strings manipulation

--- basics/05_string.js
+++ basics/05_string.js
@@ -1,4 +1,6 @@
 const name = "emily";
 const repoCount = 20;
 
-console.log(name + " " + repoCount + " My repository count");
+// console.log(name + " " + repoCount + " My repository count");
+
+console.log(`this is ${name}`);
