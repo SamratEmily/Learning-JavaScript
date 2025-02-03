68e22f67e92d066095e172ce60ff646d9712ad22
samrathossen2012 <samrat@welabs.dev>
2025-02-03 23:49:53 +0600

    added some more js functionalities

--- js/first.js
+++ js/first.js
@@ -1,8 +1,67 @@
-console.log("hello sunshine");
+console.log("Hello Sunshine! 🌞");
 
-const newButton = document.getElementsByClassName("button-for-color")[0];
-
-newButton.addEventListener("click", function(){
+// Light Toggle Button
+const lightButton = document.getElementsByClassName("button-for-light")[0];
+lightButton.addEventListener("click", function () {
     document.body.style.backgroundColor = 
-        document.body.style.backgroundColor === "green" ? "white" : "green";
+        document.body.style.backgroundColor === "pink" ? "white" : "pink";
+});
+
+// Color Animation Button
+const colorButton = document.getElementsByClassName("button-for-color")[0];
+colorButton.addEventListener("click", function () {
+    let colors = ["red", "green", "blue", "yellow", "purple"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
+
+// Toggle Text Button
+const textButton = document.getElementsByClassName("toggle-text")[0];
+const heading = document.querySelector("h1");
+
+textButton.addEventListener("click", function () {
+    heading.innerText = heading.innerText === "I am Learning JavaScript" 
+        ? "JavaScript is Fun!" 
+        : "I am Learning JavaScript";
+});
+
+// Show/Hide Message Button
+const messageButton = document.getElementsByClassName("show-message")[0];
+const message = document.querySelector(".message");
+
+messageButton.addEventListener("click", function () {
+    message.classList.toggle("hidden");
+});
+
+// Add New List Item Button
+const addItemButton = document.getElementsByClassName("add-item")[0];
+const list = document.getElementById("dynamic-list");
+
+addItemButton.addEventListener("click", function () {
+    let newItem = document.createElement("li");
+    newItem.innerText = "New Item " + (list.children.length + 1);
+    list.appendChild(newItem);
+});
+
+// Delete Last Item Button
+const deleteItemButton = document.getElementsByClassName("delete-item")[0];
+
+deleteItemButton.addEventListener("click", function () {
+    if (list.children.length > 0) {
+        list.removeChild(list.lastChild);
+    } else {
+        alert("No more items to delete!");
+    }
+});
+
+// Update Last Item Button
+const updateItemButton = document.getElementsByClassName("update-item")[0];
+
+updateItemButton.addEventListener("click", function () {
+    if (list.children.length > 0) {
+        let lastItem = list.lastChild;
+        lastItem.innerText = "Updated Item " + list.children.length;
+    } else {
+        alert("No items to update!");
+    }
 });
